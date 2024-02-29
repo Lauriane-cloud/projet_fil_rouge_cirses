dc1efd099c5134c8f3f9f1ef42a03934966d286c
Juliana-Le-Meur <144248867+Juliana-Le-Meur@users.noreply.github.com>
2024-02-29 14:48:09 +0100

    Update aug_do.py
    Ajout augmentation de donnée avec bruit blanc

--- aug_do.py
+++ aug_do.py
@@ -1,6 +1,13 @@
 import os
 import cv2
+import numpy as np
 
+def ajouter_bruit_gaussien(image, mean=0, sigma=25):
+    """Ajoute du bruit gaussien à une image."""
+    row, col, ch = image.shape
+    gauss = np.random.normal(mean, sigma, (row, col, ch))
+    bruit_gaussien = np.clip(image + gauss, 0, 255)
+    return bruit_gaussien.astype(np.uint8)
 
 # Définir le répertoire contenant les images
 input_directory_p = "../photo_cirses/Cirses.v1i.folder/Positive"
@@ -26,32 +33,68 @@ for filename in image_files:
         output_name = f"{os.path.splitext(filename)[0]}_rotated_90.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), rotated_image)
 
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_90_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
+
         # Effet miroir
         mirrored_image = cv2.flip(rotated_image, 1)
         output_name = f"{os.path.splitext(filename)[0]}_rotated_90_mirrored.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), mirrored_image)
 
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_90_mirrored_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
+
+
         rotated_image = cv2.rotate(image, cv2.ROTATE_180)
         output_name = f"{os.path.splitext(filename)[0]}_rotated_180.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), rotated_image)
+
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_180_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
 
         # Effet miroir
         mirrored_image = cv2.flip(rotated_image, 1)
         output_name = f"{os.path.splitext(filename)[0]}_rotated_180_mirrored.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), mirrored_image)
 
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_180_mirrored_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
+
         rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
         output_name = f"{os.path.splitext(filename)[0]}_rotated_270.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), rotated_image)
+
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_270_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
 
         # Effet miroir
         mirrored_image = cv2.flip(rotated_image, 1)
         output_name = f"{os.path.splitext(filename)[0]}_rotated_270_mirrored.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), mirrored_image)
+
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_rotated_270_mirrored_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
         
         # Effet miroir
         mirrored_image = cv2.flip(image, 1)
         output_name = f"{os.path.splitext(filename)[0]}_mirrored.jpg"
         cv2.imwrite(os.path.join(output_directory, output_name), mirrored_image)
+
+        # Bruit gaussien
+        white_noise_image = ajouter_bruit_gaussien(image)
+        output_name = f"{os.path.splitext(filename)[0]}_white_noise.jpg"
+        cv2.imwrite(os.path.join(output_directory, output_name), white_noise_image)
 
 print("Augmentation des données terminée.")
